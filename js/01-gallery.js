358c77692cb4df71cd647839b57c29f84bd67a83
Daniil Sorokin <99132300+Deadpul378@users.noreply.github.com>
2022-07-30 15:22:15 +0300

    ф

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+galleryEl.innerHTML = createGalleryItem(galleryItems);
+galleryEl.addEventListener("click", largeImage);
 
 console.log(galleryItems);
+function createGalleryItem(item) {
+  return item
+    .map(
+      ({ preview, original, description }) => `<div class="gallery_item">
+    <a class="gallery_link  href="${original}"></a>
+    <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}"/>
+    </div>`
+    )
+    .join("");
+}
+
+function largeImage(event) {
+  event.preventDefault();
+  const modalImg = event.target.dataset.source;
+  const instance = basicLightbox.create(template(modalImg));
+  instance.show(modalImg);
+}
+const template = (link) => `<div>
+<img src="${link}" alt="original"/>
+</div>`;
